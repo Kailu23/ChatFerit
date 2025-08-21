7cb4360e9408354f3524db5ca2ae65f419f4c7a1
Marin Valenčak <valencakmarin@gmail.com>
2025-08-21 14:25:59 +0200

    Add sign in screen logic.

--- app/src/main/java/com/example/chatferit/feature/auth/signin/SignInScreen.kt
+++ app/src/main/java/com/example/chatferit/feature/auth/signin/SignInScreen.kt
@@ -1,5 +1,6 @@
 package com.example.chatferit.feature.auth.signin
 
+import android.widget.Toast
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
@@ -10,11 +11,14 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.material3.Button
+import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
@@ -32,16 +36,35 @@ import com.example.chatferit.feature.auth.signup.SignUpScreen
 import org.w3c.dom.Text
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.setValue
-
+import androidx.compose.ui.platform.LocalContext
+import androidx.hilt.navigation.compose.hiltViewModel
 
 
 @Composable
 fun SignInScreen(navController: NavController) {
+    val viewModel : SignInViewModel = hiltViewModel()
+    val uiState = viewModel.state.collectAsState()
     var email by remember {
         mutableStateOf(value = "")
     }
     var password by remember {
         mutableStateOf(value = "")
+    }
+
+    val context = LocalContext.current
+    LaunchedEffect(key1 = uiState.value)
+    {
+        when (uiState.value) {
+            is SignInState.Success -> {
+                navController.navigate("home")
+            }
+
+            is SignInState.Error -> {
+                Toast.makeText(context, "Sign In failed", Toast.LENGTH_SHORT).show()
+            }
+
+            else -> {}
+        }
     }
     Scaffold (modifier = Modifier.fillMaxSize()){
         Column (modifier = Modifier
@@ -67,9 +90,16 @@ fun SignInScreen(navController: NavController) {
                 visualTransformation = PasswordVisualTransformation())
 
             Spacer(modifier = Modifier.size(16.dp))
-
-            Button(onClick = {/*TODO*/}, modifier = Modifier.fillMaxWidth()) {
-                Text(text = "Sign in")
+            if (uiState.value == SignInState.Loading) {
+                CircularProgressIndicator()
+            } else {
+                Button(
+                    onClick = {viewModel.SignIn(email, password)},
+                    modifier = Modifier.fillMaxWidth(),
+                    enabled = email.isNotEmpty() && password.isNotEmpty() && (uiState.value == SignInState.Nothing || uiState.value == SignInState.Error)
+                ) {
+                    Text(text = "Sign in")
+                }
             }
             TextButton(onClick = {navController.navigate("signup")}) {
                 Text(text = "Don't have an account? Sign up.")

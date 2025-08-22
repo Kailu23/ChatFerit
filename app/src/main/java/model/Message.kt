5c2f31a0c08aba2f02d345530dc83b53304f5260
Marin Valenčak <valencakmarin@gmail.com>
2025-08-22 18:37:38 +0200

    Add Channel and Message data classes

---
+++ app/src/main/java/model/Message.kt
@@ -0,0 +1,11 @@
+package com.example.chatferit.model
+
+data class Message(
+    val id : String = "",
+    val senderId : String? = "",
+    val message : String = "",
+    val senderName : String? = "",
+    val senderImage : String? = null,
+    val imageUrl : String? = null,
+    val createdAt: Long = System.currentTimeMillis()
+)

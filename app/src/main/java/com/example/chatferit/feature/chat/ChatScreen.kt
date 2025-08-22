a2fec392776ffb3e40908505cf072f8bba1f2543
Marin Valenčak <valencakmarin@gmail.com>
2025-08-22 19:23:09 +0200

    display messages in correct chronological order and align bubbles by sender in ChatScreen

--- app/src/main/java/com/example/chatferit/feature/chat/ChatScreen.kt
+++ app/src/main/java/com/example/chatferit/feature/chat/ChatScreen.kt
@@ -123,20 +123,17 @@ fun ChatBubble(message: Message) {
     } else {
         Color.Green
     }
-    Row (
+    Box (
         modifier = Modifier
             .fillMaxWidth()
             .padding(vertical = 4.dp, horizontal = 8.dp),
-        horizontalArrangement = Arrangement.spacedBy(8.dp),
-        verticalAlignment = Alignment.Bottom
     ){
-        val alignment = if (isCurrentUser) Alignment.CenterStart else Alignment.CenterEnd
-
+        val alignment = if (!isCurrentUser) Alignment.CenterStart else Alignment.CenterEnd
         Box(
-            contentAlignment = alignment,
             modifier = Modifier
                 .padding(8.dp)
                 .background(color = bubbleColor, shape = RoundedCornerShape(8.dp))
+                .align(alignment)
         ){
             Text(
                 text = message.message,

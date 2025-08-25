0250245e807814774427cb886807dbffecdb33eb
Marin Valenčak <valencakmarin@gmail.com>
2025-08-25 19:34:33 +0200

    feat(chat): send FCM topic notifications from ChatViewModel (access token via service account; Volley POST to v1 API)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -44,6 +44,7 @@ android {
     }
     packaging {
         resources {
+            excludes += "/META-INF/{AL2.0,LGPL2.1}"
             excludes += "META-INF/DEPENDENCIES"
             excludes += "META-INF/NOTICE"
             excludes += "META-INF/LICENSE"
@@ -64,6 +65,7 @@ dependencies {
     implementation(libs.androidx.ui.graphics)
     implementation(libs.androidx.ui.tooling.preview)
     implementation(libs.androidx.material3)
+    implementation(libs.volley)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
--- app/src/main/java/com/example/chatferit/feature/chat/ChatViewModel.kt
+++ app/src/main/java/com/example/chatferit/feature/chat/ChatViewModel.kt
@@ -1,31 +1,36 @@
 package com.example.chatferit.feature.chat
 
+import android.content.Context
 import android.net.Uri
-import android.widget.ImageView
-import androidx.compose.animation.core.snap
-import androidx.compose.material3.pulltorefresh.rememberPullToRefreshState
-import androidx.compose.ui.text.LinkAnnotation
-import androidx.core.app.NotificationCompat
+import android.util.Log
 import androidx.lifecycle.ViewModel
+import com.android.volley.Response
+import com.android.volley.toolbox.StringRequest
+import com.android.volley.toolbox.Volley
+import com.example.chatferit.R
 import com.example.chatferit.model.Message
+import com.google.auth.oauth2.GoogleCredentials
 import com.google.firebase.Firebase
 import com.google.firebase.auth.auth
 import com.google.firebase.database.DataSnapshot
 import com.google.firebase.database.DatabaseError
 import com.google.firebase.database.FirebaseDatabase
 import com.google.firebase.database.ValueEventListener
+import com.google.firebase.messaging.FirebaseMessaging
 import com.google.firebase.storage.FirebaseStorage
-import com.google.firebase.storage.storage
 import dagger.hilt.android.lifecycle.HiltViewModel
+import dagger.hilt.android.qualifiers.ApplicationContext
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.asStateFlow
+import org.json.JSONObject
 import java.util.UUID
 import javax.inject.Inject
 
 @HiltViewModel
 class ChatViewModel @Inject constructor(
     private val firebaseDatabase : FirebaseDatabase,
-    private val firebaseStorage: FirebaseStorage
+    private val firebaseStorage: FirebaseStorage,
+    @ApplicationContext private val context: Context
 ) : ViewModel() {
     private val _messages = MutableStateFlow<List<Message>>(emptyList())
     val messages = _messages.asStateFlow()
@@ -40,7 +45,11 @@ class ChatViewModel @Inject constructor(
             imageUrl = image
         )
 
-        firebaseDatabase.reference.child("messages").child(channelId).push().setValue(message)
+        firebaseDatabase.reference.child("messages").child(channelId).push().setValue(message).addOnCompleteListener { task ->
+            if(task.isSuccessful) {
+                PostNotificationToUsers(channelId = channelId, senderName = message.senderName?: "", messageContent = message.message)
+            }
+        }
     }
 
     fun SendImageMessage(uri: Uri, channelID: String) {
@@ -78,6 +87,57 @@ class ChatViewModel @Inject constructor(
                     TODO("Not yet implemented")
                 }
             })
+        SubscribeForNotification(channelId)
+    }
+    private fun SubscribeForNotification(channelId : String) {
+        FirebaseMessaging.getInstance().subscribeToTopic("group_$channelId").addOnCompleteListener {
+            if (it.isSuccessful) {
+                Log.d("ChatViewMOdel", "Subscribed to topic: group_$channelId")
+            } else {
+                Log.d("ChatViewMOdel", "Failed to subscribe to topic: group_$channelId")
+            }
+        }
+    }
 
+    fun PostNotificationToUsers(channelId: String, senderName : String, messageContent: String) {
+        val fcmUrl = "https://fcm.googleapis.com/v1/projects/chatferit/messages:send"
+        val jsonBody = JSONObject().apply {
+            put("message", JSONObject().apply {
+                put("topic", "group_$channelId")
+                put("notification", JSONObject().apply {
+                    put("title", channelId)
+                    put("body", "$senderName: $messageContent")
+                })
+            })
+        }
+
+        val requestBody = jsonBody.toString()
+
+        val request = object : StringRequest(Method.POST, fcmUrl,Response.Listener {
+                Log.d("ChatViewModel", "Notification sent successfully")
+            },Response.ErrorListener {
+                Log.e("ChatViewModel", "Failed to send notification")
+            }) {
+            override fun getBody(): ByteArray? {
+                return requestBody.toByteArray()
+            }
+
+            override fun getHeaders(): MutableMap<String, String> {
+                val headers = HashMap<String, String>()
+                headers["Authorization"] = "Bearer ${GetAccessToken()}"
+                headers["Content-Type"] = "application/json"
+                return headers
+            }
+        }
+        val queue = Volley.newRequestQueue(context)
+        queue.add(request)
+
+    }
+
+    private fun GetAccessToken() : String {
+        val inputStream = context.resources.openRawResource(R.raw.chatferit_key)
+        val googleCredentials = GoogleCredentials.fromStream(inputStream)
+            .createScoped(listOf("https://www.googleapis.com/auth/firebase.messaging"))
+        return googleCredentials.refreshAccessToken().tokenValue
     }
 }

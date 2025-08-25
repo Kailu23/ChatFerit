0125cb600181354e806c7a6b0c705e0b03f6837b
Marin Valenčak <valencakmarin@gmail.com>
2025-08-25 13:48:43 +0200

    feature(chat): implement picture sharing dialogs in ChatScreen

--- app/src/main/java/com/example/chatferit/feature/chat/ChatScreen.kt
+++ app/src/main/java/com/example/chatferit/feature/chat/ChatScreen.kt
@@ -1,10 +1,11 @@
 package com.example.chatferit.feature.chat
 
-import com.example.chatferit.R
+import android.net.Uri
+import android.os.Environment
+import androidx.activity.compose.rememberLauncherForActivityResult
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
-import androidx.compose.foundation.gestures.snapping.SnapPosition
-import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
@@ -19,38 +20,40 @@ import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.text.KeyboardActions
 import androidx.compose.foundation.text.KeyboardOptions
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.automirrored.filled.Send
-import androidx.compose.material3.Icon
+import androidx.compose.material3.AlertDialog
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
 import androidx.compose.material3.TextField
 import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
-import androidx.compose.runtime.currentCompositionLocalContext
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.modifier.modifierLocalOf
 import androidx.compose.ui.platform.LocalSoftwareKeyboardController
-import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.input.ImeAction
-import androidx.compose.ui.text.style.LineHeightStyle
 import androidx.compose.ui.unit.dp
+import androidx.core.content.ContextCompat
+import androidx.core.content.FileProvider
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavController
+import com.example.chatferit.R
 import com.example.chatferit.model.Message
 import com.example.chatferit.ui.theme.DarkGray
 import com.example.chatferit.ui.theme.Purple
 import com.google.firebase.Firebase
 import com.google.firebase.auth.auth
-import org.w3c.dom.Text
+import java.io.File
+import java.text.SimpleDateFormat
+import java.util.Date
+import java.util.Locale
+import android.Manifest
+import androidx.compose.ui.Alignment
 
 @Composable
 fun ChatScreen(navController: NavController, channelId : String) {
@@ -60,7 +63,39 @@ fun ChatScreen(navController: NavController, channelId : String) {
         containerColor = Color.Black
     ){
 
+        val selectDialog = remember { mutableStateOf(false) }
+        val cameraImageUri = remember { mutableStateOf<Uri?>(null) }
+        val cameraImageLauncher =
+            rememberLauncherForActivityResult(contract = ActivityResultContracts.TakePicture()) {
+                success ->
+                if (success) {
+                    cameraImageUri.value?.let {
+                        /*TODO()*/
+                    }
+                }
+            }
 
+        fun createImageUri(): Uri {
+            val timeStamp = SimpleDateFormat("yyyyMMdd_HHmmss", Locale.getDefault()).format(Date())
+            val storageDir =
+                ContextCompat.getExternalFilesDirs(
+                    navController.context,
+                    Environment.DIRECTORY_PICTURES
+                ).first()
+            return FileProvider.getUriForFile(
+                navController.context,
+                "${navController.context.packageName}.provider",
+                File.createTempFile("JPEG_${timeStamp}_", ".jpg", storageDir).apply {
+                    cameraImageUri.value = Uri.fromFile(this)
+                }
+            )
+        }
+        val permissionLauncher =
+            rememberLauncherForActivityResult(contract = ActivityResultContracts.RequestPermission()) { isGranted ->
+                if (isGranted) {
+                    cameraImageLauncher.launch(createImageUri())
+                }
+            }
 
         Column (modifier = Modifier
             .fillMaxSize()
@@ -76,17 +111,59 @@ fun ChatScreen(navController: NavController, channelId : String) {
                 messages = messages.value,
                 onSendMessage = {message ->
                     viewModel.sendMessage(channelId, message)
-                }
+                },
+                onImageClicked = {selectDialog.value = true}
             )
         }
+
+
+        if (selectDialog.value) {
+            ContentSelectionDialog(onCameraSelected = {
+                selectDialog.value = false
+                if (navController.context.checkSelfPermission(Manifest.permission.CAMERA) == android.content.pm.PackageManager.PERMISSION_GRANTED) {
+                    cameraImageLauncher.launch(createImageUri())
+                } else {
+                    permissionLauncher.launch(Manifest.permission.CAMERA)
+                }
+
+            }, onGallerySelected = {
+                selectDialog.value = false
+//                imageLauncher.launch("image/*")
+            })
+        }
     }
+}
+
+@Composable
+fun ContentSelectionDialog(onCameraSelected: () -> Unit, onGallerySelected: () -> Unit) {
+    AlertDialog(onDismissRequest = {/*TODO()*/}, confirmButton = {
+        TextButton (onClick = onCameraSelected) {
+            Text(
+                text = "Camera",
+                color = DarkGray
+            )
+        }},
+        dismissButton = {
+            TextButton (onClick = onCameraSelected) {
+                Text(
+                    text = "Gallery",
+                    color = DarkGray
+                )
+            }
+            }, title = {Text(text = "Select your source.")},
+        text = {Text(text = "Would you like to pick an image from the gallery or use the camera?") })
+
+
+
+
 
 }
 
 @Composable
 fun ChatMessages(
     messages : List<Message>,
-    onSendMessage : (String) -> Unit
+    onSendMessage : (String) -> Unit,
+    onImageClicked: () -> Unit
 ) {
     val message = remember { mutableStateOf("") }
 
@@ -107,10 +184,13 @@ fun ChatMessages(
                 .align(Alignment.BottomCenter)
                 .background(DarkGray)
                 .padding(8.dp),
-            verticalAlignment = Alignment.Bottom
+            verticalAlignment = Alignment.CenterVertically
         ){
             IconButton(
-                onClick = { /*TODO()*/ }
+                onClick = {
+                    message.value = ""
+                    onImageClicked()
+                }
             ) {
                 Image(painter = painterResource(R.drawable.outline_attach_file_24), contentDescription = "Attach File", alignment = Alignment.Center, modifier = Modifier.size(24.dp))
             }

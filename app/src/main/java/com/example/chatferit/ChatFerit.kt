de90766ae7d9bdde624e41d736345530bcfc74b4
Marin Valenčak <valencakmarin@gmail.com>
2025-08-30 00:50:10 +0200

    feat(notification): initialize notification channels on app start and configure Hilt Application

--- app/src/main/java/com/example/chatferit/ChatFerit.kt
+++ app/src/main/java/com/example/chatferit/ChatFerit.kt
@@ -1,8 +1,15 @@
 package com.example.chatferit
 
 import android.app.Application
+import com.example.chatferit.notifications.NotificationHelper
 import dagger.hilt.android.HiltAndroidApp
 
 @HiltAndroidApp
 class ChatFerit : Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+
+        NotificationHelper.createNotificationChannel(this)
+    }
 }
